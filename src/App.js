087c08eceb7178e8567874e6d1ff7ed43e33540b
KirbyPatrick <pkirbyinatx@gmail.com>
2019-06-16 08:25:31 -0500

    began working on calculators grid format

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper center">
+      <div className="gridClass">
+        <Button>test</Button>
+        <Button>test</Button>
+        <Button>test</Button>
+        <Button>test</Button>
+        <Button>test</Button>
+        <Button>test</Button>
+        <Button>test</Button>
+        <Button>test</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
