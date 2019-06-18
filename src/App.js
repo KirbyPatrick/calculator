b1b9e1a381f72e233223c7781629045e3ff8b372
KirbyPatrick <pkirbyinatx@gmail.com>
2019-06-18 06:55:06 -0500

    combined Results component with CalculatorButtons.js and deleted Results.js. added additional styling to operator buttons via Material UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 import CalculatorButtons from "./components/CalculatorButtons";
-import Results from "./components/Results";
 
 function App() {
   return (
     <div className="wrapper center">
-      <Results />
       <CalculatorButtons />
     </div>
   );
--- src/components/calculatorButtons.js
+++ src/components/calculatorButtons.js
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
 
-const CalculatorButtons = () => {
-  return (
-    <div className="gridClass">
-      <Button>7</Button>
-      <Button>8</Button>
-      <Button>9</Button>
-      <Button>/</Button>
-      <Button>4</Button>
-      <Button>5</Button>
-      <Button>6</Button>
-      <Button>*</Button>
-      <Button>1</Button>
-      <Button>2</Button>
-      <Button>3</Button>
-      <Button>-</Button>
-      <Button>0</Button>
-      <Button>.</Button>
-      <Button>ENTER</Button>
-      <Button>+</Button>
-    </div>
-  );
-};
+class CalculatorButtons extends Component {
+  state = {
+    currentNumber: 0
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <p>{this.state.currentNumber}</p>
+        </div>
+        <div className="gridClass">
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Fab aria-label="Add" className="fab">
+            /
+          </Fab>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Fab aria-label="Add" className="fab">
+            *
+          </Fab>
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Fab aria-label="Add" className="fab">
+            -
+          </Fab>
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>ENTER</Button>
+          <Fab aria-label="Add">+</Fab>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default CalculatorButtons;
