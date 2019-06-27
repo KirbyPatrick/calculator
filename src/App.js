076a24b52f65824573ecad7b3f0eca45ca59a1b9
KirbyPatrick <pkirbyinatx@gmail.com>
2019-06-26 20:09:12 -0500

    passed props from CalculatorButtons.js to App.js and added state to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import CalculatorButtons from "./components/CalculatorButtons";
+import ResultComponent from "./components/ResultComponent";
 
-function App() {
-  return (
-    <div className="wrapper center">
-      <CalculatorButtons />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: ""
+    };
+  }
+
+  render() {
+    return (
+      <div className="wrapper center">
+        <ResultComponent result={this.state.result} />
+        <CalculatorButtons onClick={this.onClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/calculatorButtons.js
+++ src/components/calculatorButtons.js
@@ -3,36 +3,69 @@ import Button from "@material-ui/core/Button";
 import Fab from "@material-ui/core/Fab";
 
 class CalculatorButtons extends Component {
-  
   render() {
     return (
       <div>
         <div className="gridClass">
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)}>7</Button>
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)}>8</Button>
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)}>9</Button>
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)} aria-label="Add" className="fab">
+          {/* first row of numbers */}
+
+          <Button name="7" onClick={e => this.props.onClick(e.target.name)}>
+            7
+          </Button>
+          <Button name="8" onClick={e => this.props.onClick(e.target.name)}>
+            8
+          </Button>
+          <Button name="9" onClick={e => this.props.onClick(e.target.name)}>
+            9
+          </Button>
+          <Button name="/" onClick={e => this.props.onClick(e.target.name)}>
             /
           </Button>
 
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)}>4</Button>
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)}>5</Button>
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)}>6</Button>
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)} aria-label="Add" className="fab">
+          {/* second row of numbers */}
+
+          <Button name="4" onClick={e => this.props.onClick(e.target.name)}>
+            4
+          </Button>
+          <Button name="5" onClick={e => this.props.onClick(e.target.name)}>
+            5
+          </Button>
+          <Button name="6" onClick={e => this.props.onClick(e.target.name)}>
+            6
+          </Button>
+          <Button name="*" onClick={e => this.props.onClick(e.target.name)}>
             *
           </Button>
 
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)}>1</Button>
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)}>2</Button>
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)}>3</Button>
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)} aria-label="Add" className="fab">
+          {/* third row of numbers */}
+
+          <Button name="1" onClick={e => this.props.onClick(e.target.name)}>
+            1
+          </Button>
+          <Button name="2" onClick={e => this.props.onClick(e.target.name)}>
+            2
+          </Button>
+          <Button name="3" onClick={e => this.props.onClick(e.target.name)}>
+            3
+          </Button>
+          <Button name="-" onClick={e => this.props.onClick(e.target.name)}>
             -
           </Button>
 
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)}>0</Button>
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)}>.</Button>
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)}>ENTER</Button>
-          <Button name="7" onClick={e => this.props.onClick(e.target.name)} aria-label="Add">+</Button>
+          {/* forth row of numbers */}
+
+          <Button name="0" onClick={e => this.props.onClick(e.target.name)}>
+            0
+          </Button>
+          <Button name="." onClick={e => this.props.onClick(e.target.name)}>
+            .
+          </Button>
+          <Button name="Enter" onClick={e => this.props.onClick(e.target.name)}>
+            ENTER
+          </Button>
+          <Button name="+" onClick={e => this.props.onClick(e.target.name)}>
+            +
+          </Button>
         </div>
       </div>
     );
